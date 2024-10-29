733275663b4618f63aeb1d1bf9d8211df0f21590
Justin Fanton <justin.fanton@gmail.com>
2024-10-29 14:43:44 +0100

    Implement homepage (Codealong)

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Homepage() {
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-crud-api.herokuapp.com/characters")
+      .then((response) => {
+        setCharacters(response.data);
+      })
+      .catch((e) => {
+        console.log("Error getting characters from the API");
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>List of characters in our API</h2>
+
+      { characters === null
+            ? <h2>Loading...</h2>
+            : <h2>Numbers of characters in our API: {characters.length}</h2>
+      }
+
+      {/* characters && ensures to check if the new array is truthy using the useState property from earlier (Null is automatically falsy, and so if the new array returns with any info it is then truthy) */}
+      {characters &&
+        characters.map((characterDetails, index) => {
+          return (
+            <div key={index} className="box">
+              <h3>Character's name: {characterDetails.name}</h3>
+              <p>Character's name: {characterDetails.occupation}</p>
+              <p>Weapon of choice: {characterDetails.weapon}</p>
+            </div>
+          );
+        })}
+    </>
+  );
+}
+
+export default Homepage;
